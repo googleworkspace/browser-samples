364d2fcb8c637529e718b2309873526664edd60e
Grant Timmerman <timmerman+devrel@google.com>
2019-05-15 19:53:30 -0700

    refactor: Updates all quickstarts to modern JS. Improves code consistency.

--- apps-script/execute/index.js
+++ apps-script/execute/index.js
@@ -19,19 +19,19 @@
  * Load the API and make an API call.  Display the results on the screen.
  */
 function callScriptFunction() {
-  var scriptId = "<ENTER_YOUR_SCRIPT_ID_HERE>";
+  const scriptId = "<ENTER_YOUR_SCRIPT_ID_HERE>";
 
   // Call the Apps Script API run method
   //   'scriptId' is the URL parameter that states what script to run
   //   'resource' describes the run request body (with the function name
   //              to execute)
   gapi.client.script.scripts.run({
-    'scriptId': scriptId,
-    'resource': {
-      'function': 'getFoldersUnderRoot'
+    scriptId,
+    resource: {
+      function: 'getFoldersUnderRoot'
     }
-  }).then(function(resp) {
-    var result = resp.result;
+  }).then((resp) => {
+    const result = resp.result;
     if (result.error && result.error.status) {
       // The API encountered a problem before the script
       // started executing.
@@ -43,31 +43,29 @@ function callScriptFunction() {
       // Extract the first (and only) set of error details.
       // The values of this object are the script's 'errorMessage' and
       // 'errorType', and an array of stack trace elements.
-      var error = result.error.details[0];
-      appendPre('Script error message: ' + error.errorMessage);
+      const error = result.error.details[0];
+      appendPre(`Script error message: ${error.errorMessage}`);
 
       if (error.scriptStackTraceElements) {
         // There may not be a stacktrace if the script didn't start
         // executing.
         appendPre('Script error stacktrace:');
-        for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
-          var trace = error.scriptStackTraceElements[i];
-          appendPre('\t' + trace.function + ':' + trace.lineNumber);
-        }
+        error.scriptStackTraceElements.map((trace) => {
+          appendPre(`\t${trace.function}:${trace.lineNumber}`);
+        });
       }
     } else {
       // The structure of the result will depend upon what the Apps
       // Script function returns. Here, the function returns an Apps
       // Script Object with String keys and values, and so the result
       // is treated as a JavaScript object (folderSet).
-
-      var folderSet = result.response.result;
+      const folderSet = result.response.result;
       if (Object.keys(folderSet).length == 0) {
           appendPre('No folders returned!');
       } else {
         appendPre('Folders under your root folder:');
-        Object.keys(folderSet).forEach(function(id){
-          appendPre('\t' + folderSet[id] + ' (' + id  + ')');
+        Object.keys(folderSet).forEach((id) => {
+          appendPre(`\t${folderSet[id]} (${id})`);
         });
       }
     }
