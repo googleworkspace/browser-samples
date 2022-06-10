378534037acab14e73ee07ce4ce021beed974d39
suchawla09 <suchawla@google.com>
2022-06-10 17:48:40 +0530

    Slides snippets added with exceptional handling

---
+++ slides/snippets/slides_create_image.js
@@ -0,0 +1,67 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [START slides_create_image]
+function createImage(presentationId, pageId, IMAGE_URL,callback) {
+
+  var imageUrl = IMAGE_URL;
+
+  // Create a new image, using the supplied object ID, with content downloaded from imageUrl.
+  let requests = [];
+  const imageId = 'MyImage_02';
+  let emu4M = {
+    magnitude: 4000000,
+    unit: 'EMU'
+  };
+  requests.push({
+    createImage: {
+      objectId: imageId,
+      url: imageUrl,
+      elementProperties: {
+        pageObjectId: pageId,
+        size: {
+          height: emu4M,
+          width: emu4M
+        },
+        transform: {
+          scaleX: 1,
+          scaleY: 1,
+          translateX: 100000,
+          translateY: 100000,
+          unit: 'EMU'
+        }
+      }
+    }
+  });
+
+  // Execute the request.
+  gapi.client.slides.presentations.batchUpdate({
+    presentationId: presentationId,
+    requests: requests
+  }).then((response) => {
+  try
+  {
+    let createImageResponse = response.result.replies;
+    console.log(`Created image with ID: ${createImageResponse[0].createImage.objectId}`);
+    // [START_EXCLUDE silent]
+    if(callback) callback(createImageResponse);
+    }
+  catch(ex)
+  {
+   console.log("API returned an error in slides_create_image",ex.message);
+   return;
+   }
+    // [END_EXCLUDE]
+  });
+  // [END slides_create_image]
+}
---
+++ slides/snippets/slides_create_textbox_with_text.js
@@ -0,0 +1,73 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [START slides_create_textbox_with_text]
+function createTextboxWithText(presentationId, pageId, callback) {
+
+  // Create a new square textbox, using the supplied element ID.
+  const elementId = 'MyTextBox_01';
+  let pt350 = {
+    magnitude: 350,
+    unit: 'PT'
+  };
+  let requests = [{
+    createShape: {
+      objectId: elementId,
+      shapeType: 'TEXT_BOX',
+      elementProperties: {
+        pageObjectId: pageId,
+        size: {
+          height: pt350,
+          width: pt350
+        },
+        transform: {
+          scaleX: 1,
+          scaleY: 1,
+          translateX: 350,
+          translateY: 100,
+          unit: 'PT'
+        }
+      }
+    }
+  },
+
+  // Insert text into the box, using the supplied element ID.
+  {
+    insertText: {
+      objectId: elementId,
+      insertionIndex: 0,
+      text: 'New Box Text Inserted!'
+    }
+  }];
+
+  // Execute the request.
+  gapi.client.slides.presentations.batchUpdate({
+    presentationId: presentationId,
+    requests: requests
+  }).then((createTextboxWithTextResponse) => {
+  try
+  {
+    let createShapeResponse = createTextboxWithTextResponse.result.replies[0].createShape;
+    console.log(`Created textbox with ID: ${createShapeResponse.objectId}`);
+    // [START_EXCLUDE silent]
+    if(callback) callback(createTextboxWithTextResponse.result);
+    // [END_EXCLUDE]
+    }
+  catch(ex)
+  {
+   console.log("API returned an error in slides_create_textbox_with_text",ex.message);
+   return;
+   }
+  });
+  // [END slides_create_textbox_with_text]
+}
