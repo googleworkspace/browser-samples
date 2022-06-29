c567b2bb2926d26e0fa5276726160910f8098daf
suchawla09 <suchawla@google.com>
2022-06-29 14:26:15 +0530

    test: unit test case for sheet snippets

--- sheets/snippets/test_batch_update_spreadsheet_values.js
+++ sheets/snippets/test_batch_update_spreadsheet_values.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 function testBatchUpdateSpreadsheetValues(done) {
   createTestSpreadsheet(function(spreadsheetId) {
     batchUpdateValues(spreadsheetId, 'A1:B2', 'USER_ENTERED', [
--- sheets/snippets/test_update_spreadsheet_values.js
+++ sheets/snippets/test_update_spreadsheet_values.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 function testUpdateSpreadsheetValues(done) {
   createTestSpreadsheet(function(spreadsheetId) {
     updateValues(spreadsheetId, 'A1:B2', 'USER_ENTERED', [
