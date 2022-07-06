cb098290be5c388b93c0801db22a40fa63968e2e
suchawla09 <102661233+suchawla09@users.noreply.github.com>
2022-07-07 03:11:52 +0530

    feat: browser samples slides-snippets updated (#120)
    * sheet region tag added and index.html files updated
    
    * Slides_snippets added
    
    * Slides_snippets added
    
    * Slides snippets updated
    
    * Slides snippets updated
    
    * Slides snippets updated
    
    * feat: browser samples slides-snippets updated
    
    Co-authored-by: Rajesh Mudaliyar <mudaliyar@google.com>

---
+++ slides/snippets/slides_create_textbox_with_text.js
@@ -0,0 +1,66 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [START slides_create_textbox_with_text]
+function createTextboxWithText(presentationId, pageId, callback) {
+  // Create a new square textbox, using the supplied element ID.
+  const elementId = 'MyTextBox_01';
+  let pt350 = {
+    magnitude: 350,
+    unit: 'PT'
+  };
+  let requests = [{
+    createShape: {
+      objectId: elementId,
+      shapeType: 'TEXT_BOX',
+      elementProperties: {
+        pageObjectId: pageId,
+        size: {
+          height: pt350,
+          width: pt350
+        },
+        transform: {
+          scaleX: 1,
+          scaleY: 1,
+          translateX: 350,
+          translateY: 100,
+          unit: 'PT'
+        }
+      }
+    }
+  },
+
+  // Insert text into the box, using the supplied element ID.
+  {
+    insertText: {
+      objectId: elementId,
+      insertionIndex: 0,
+      text: 'New Box Text Inserted!'
+    }
+  }];
+  // Execute the request.
+  try {
+      gapi.client.slides.presentations.batchUpdate({
+        presentationId: presentationId,
+        requests: requests
+      }).then((createTextboxWithTextResponse) => {
+        let createShapeResponse = createTextboxWithTextResponse.result.replies[0].createShape;
+        console.log(`Created textbox with ID: ${createShapeResponse.objectId}`);
+        if(callback) callback(createTextboxWithTextResponse.result);
+      });
+  } catch(err) {
+    document.getElementById('content').innerText = err.message;
+    return;
+  }
+}
+// [END slides_create_textbox_with_text]
