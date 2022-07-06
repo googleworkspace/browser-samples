c593376f83cefe96e16758e1003d28f48ae5723b
suchawla09 <102661233+suchawla09@users.noreply.github.com>
2022-07-07 03:11:05 +0530

    test: unit test case for sheet snippets (#118)
    * sheet region tag added and index.html files updated
    
    * UT:Sheet Snippets Added
    
    * UT:Sheet Snippets Added
    
    * test: unit test case for sheet snippets

---
+++ sheets/snippets/test_append_spreadsheet_values.js
@@ -0,0 +1,31 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+function testAppendSpreadsheetValues(done) {
+  createTestSpreadsheet(function(spreadsheetId) {
+    populateValues(spreadsheetId, function(spreadsheetId) {
+      appendValues(spreadsheetId, 'Sheet1', 'USER_ENTERED', [
+        ['A', 'B'],
+        ['C', 'D']
+      ], function(response) {
+        assert.equal(response.result.tableRange, 'Sheet1!A1:J10');
+        let updates = response.result.updates;
+        assert.equal(updates.updatedRows, 2);
+        assert.equal(updates.updatedColumns, 2);
+        assert.equal(updates.updatedCells, 4);
+        done();
+      });
+    });
+  });
+}
