cda095957dc0434b76eafe58c3fe42f39a94f597
suchawla09 <102661233+suchawla09@users.noreply.github.com>
2022-07-07 03:12:16 +0530

    feat: browser samples sheet-snippets updated (#121)
    * sheet region tag added and index.html files updated
    
    * Sheets: Snippets Added
    
    * index.html updated
    
    * Sheets_Snippets added
    
    * sheets snippets updated
    
    * Sheets snippets updated
    
    * Sheet-snippets updated
    
    * feat: browser samples sheet-snippets updated
    
    Co-authored-by: Rajesh Mudaliyar <mudaliyar@google.com>

---
+++ sheets/snippets/sheets_append_values.js
@@ -0,0 +1,43 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START sheets_append_values]
+function appendValues(spreadsheetId, range, valueInputOption, _values, callback) {
+  let values = [
+    [
+      // Cell values ...
+    ],
+    // Additional rows ...
+  ];
+  values = _values;
+  const body = {
+    values: values
+  };
+  try {
+      gapi.client.sheets.spreadsheets.values.append({
+         spreadsheetId: spreadsheetId,
+         range: range,
+         valueInputOption: valueInputOption,
+         resource: body
+      }).then((response) => {
+        const result = response.result;
+        console.log(`${result.updates.updatedCells} cells appended.`)
+        if (callback) callback(response);
+      });
+  } catch(err) {
+    document.getElementById('content').innerText = err.message;
+    return;
+  }
+ }
+// [END sheets_append_values]
---
+++ sheets/snippets/sheets_batch_update_values.js
@@ -0,0 +1,48 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [START sheets_batch_update_values]
+function batchUpdateValues(spreadsheetId, range, valueInputOption, _values, callback) {
+  let values = [
+    [
+      // Cell values ...
+    ],
+    // Additional rows ...
+  ];
+  values = _values;
+  let data = [];
+  data.push({
+    range: range,
+    values: values
+  });
+  // Additional ranges to update.
+
+  let body = {
+    data: data,
+    valueInputOption: valueInputOption
+  };
+  try {
+      gapi.client.sheets.spreadsheets.values.batchUpdate({
+         spreadsheetId: spreadsheetId,
+         resource: body
+      }).then((response) => {
+        let result = response.result;
+        console.log(`${result.totalUpdatedCells} cells updated.`);
+        if(callback) callback(response);
+      });
+  } catch(err) {
+    document.getElementById('content').innerText = err.message;
+    return;
+  }
+}
+//  [END sheets_batch_update_values]
